2cd879bbdb3d27ffd9ea0bae018f81498d64402c
Shimaa <eng.shimaa@gmail.com>
2011-11-12 22:48:32 -0800

    first commit

---
+++ application/controllers/welcome.php
@@ -0,0 +1,41 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+		
+	}
+	
+	public function test_db ()
+	{
+		$this->load->database();
+		$query = $this->db->query("SELECT * FROM Country;");
+		
+		foreach ($query->result() as $row)
+		{
+			echo $row->arabic_name;
+			echo $row->english_name;
+			
+		}
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
